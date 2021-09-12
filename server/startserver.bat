906bc8aa263e2b63c60e0ecc35ff5e7ddc7d225a
voruti <45130096+voruti@users.noreply.github.com>
2021-09-12 13:33:04 +0200

    Update startserver

--- server/startserver.bat
+++ server/startserver.bat
@@ -9,10 +9,9 @@ REM
 REM The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 REM
 REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
 @ECHO OFF
 SETLOCAL
+
 
 :BEGIN
 CLS
@@ -22,19 +21,41 @@ REM Make batch directory the same as the directory it's being called from
 REM For example, if "run as admin" the batch starting dir could be system32
 CD "%~dp0" >nul 2>&1
 
+
+:CHECKJAVA
+ECHO INFO: Checking java installation...
+ECHO.
+
+REM If no Java is installed this line will catch it simply
+java -version >nul 2>&1 
+ECHO.
+IF %ERRORLEVEL% EQU 0 (
+	ECHO INFO: Found Java.
+	GOTO CHECK
+) ELSE (
+    GOTO JAVAERROR
+)
+
+:MAIN
+java -jar serverstarter-2.0.1.jar
+GOTO EOF
+
 :CHECK
 REM Check if serverstarter JAR is already downloaded
-IF NOT EXIST "%cd%\serverstarter-@@serverstarter-libVersion@@.jar" (
+IF NOT EXIST "%cd%\serverstarter-2.0.1.jar" (
 	ECHO serverstarter binary not found, downloading serverstarter...
-	%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/Yoosk/ServerStarter/releases/download/v@@serverstarter-libVersion@@/serverstarter-@@serverstarter-libVersion@@.jar "%cd%\serverstarter-@@serverstarter-libVersion@@.jar"
+	%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/AllTheMods/alltheservers/releases/download/2.0.1/serverstarter-2.0.1.jar "%cd%\serverstarter-2.0.1.jar"
    GOTO MAIN
 ) ELSE (
    GOTO MAIN
 )
 
-:MAIN
-java -jar serverstarter-@@serverstarter-libVersion@@.jar
-GOTO EOF
+
+:JAVAERROR
+COLOR CF
+ECHO ERROR: Could not find Java installed or in PATH
+PAUSE
+
 
 :EOF
 pause
--- server/startserver.sh
+++ server/startserver.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # https://github.com/AllTheMods/alltheservers
 #
 # The MIT License (MIT)
@@ -9,45 +11,27 @@
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+set -euo pipefail
 
+# check for serverstarter jar
+if [[ -f serverstarter-2.0.1.jar ]]; then
+    echo "Skipping download. Using existing serverstarter-2.0.1.jar"
+    java -jar serverstarter-2.0.1.jar
+    exit 0
+else
+    # download missing serverstarter jar
+    URL="https://github.com/AllTheMods/alltheservers/releases/download/2.0.1/serverstarter-2.0.1.jar"
 
-DO_RAMDISK=0
-if [[ $(cat server-setup-config.yaml | grep 'ramDisk:' | awk 'BEGIN {FS=":"}{print $2}') =~ "yes" ]]; then
-    SAVE_DIR=$(cat server.properties | grep 'level-name' | awk 'BEGIN {FS="="}{print $2}')
-    mv $SAVE_DIR "${SAVE_DIR}_backup"
-    mkdir $SAVE_DIR
-    sudo mount -t tmpfs -o size=2G tmpfs $SAVE_DIR
-    DO_RAMDISK=1
+    if command -v wget &> /dev/null; then
+        echo "DEBUG: (wget) Downloading ${URL}"
+        wget -O serverstarter-2.0.1.jar "${URL}"
+    elif command -v curl &> /dev/null; then
+        echo "DEBUG: (curl) Downloading ${URL}"
+        curl -o serverstarter-2.0.1.jar "${URL}"
+    else
+        echo "Neither wget or curl were found on your system. Please install one and try again"
+        exit 1
+    fi
 fi
-	if [ -f serverstarter-@@serverstarter-libVersion@@.jar ]; then
-			echo "Skipping download. Using existing serverstarter-@@serverstarter-libVersion@@.jar"
-         java -jar serverstarter-@@serverstarter-libVersion@@.jar
-               if [[ $DO_RAMDISK -eq 1 ]]; then
-               sudo umount $SAVE_DIR
-               rm -rf $SAVE_DIR
-               mv "${SAVE_DIR}_backup" $SAVE_DIR
-               fi
-               exit 0
-	else
-			export URL="https://github.com/Yoosk/ServerStarter/releases/download/v@@serverstarter-libVersion@@/serverstarter-@@serverstarter-libVersion@@.jar"
-	fi
-		echo $URL
-		which wget >> /dev/null
-		if [ $? -eq 0 ]; then
-			echo "DEBUG: (wget) Downloading ${URL}"
-			wget -O serverstarter-@@serverstarter-libVersion@@.jar "${URL}"
-   else
-			which curl >> /dev/null
-			if [ $? -eq 0 ]; then
-				echo "DEBUG: (curl) Downloading ${URL}"
-				curl -o serverstarter-@@serverstarter-libVersion@@.jar "${URL}"
-			else
-				echo "Neither wget or curl were found on your system. Please install one and try again"
-         fi
-      fi
-java -jar serverstarter-@@serverstarter-libVersion@@.jar
-if [[ $DO_RAMDISK -eq 1 ]]; then
-    sudo umount $SAVE_DIR
-    rm -rf $SAVE_DIR
-    mv "${SAVE_DIR}_backup" $SAVE_DIR
-fi
+
+java -jar serverstarter-2.0.1.jar
